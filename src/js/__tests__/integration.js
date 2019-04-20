1916885b7e09515ab42e2471da31cce6b6747863
ashwin <ashwinkn@hotmail.com>
2019-04-20 10:25:18 -0500

    generated by     - gen-lib@1.2.22     - @kurlytail/gen-sgen@1.2.21

---
+++ src/js/__tests__/integration.js
@@ -0,0 +1,25 @@
+import { execSync } from 'child_process';
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/github-app.min.js -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run github-app commands', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync(
+            'sgen -g `pwd`/dist/github-app.min.js -d src/test/fixture/design.json -o testoutput'
+        ).toString();
+        output = output.replace(/info: Loaded generator .*github-app.min.js.*/, '');
+        expect(output).toMatchSnapshot();
+        execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run build', { cwd: 'testoutput', stdio: 'inherit' });
+    });
+});
