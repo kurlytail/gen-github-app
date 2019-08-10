1f90c103d73e11717b6447798e921867d9750e8f
ashwin <ashwinkn@hotmail.com>
2019-08-10 00:09:47 -0500

    Updated test

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -3,6 +3,11 @@ import { execSync } from 'child_process';
 describe('# integration test', () => {
     beforeEach(() => {
         execSync('rm -rf testoutput');
+        execSync('mkdir testoutput');
+        execSync('git init', { cwd : 'testoutput' });
+        execSync('git config user.email "you@example.com"', { cwd: 'testoutput' });
+        execSync('git config user.name "Your Namer"', { cwd: 'testoutput' });
+        execSync('git commit --allow-empty -m "Empty commit."', { cwd: 'testoutput' });
     });
 
     it('## should print help options', () => {
@@ -17,10 +22,16 @@ describe('# integration test', () => {
             'sgen -g npm -g `pwd`/dist/github-app.min.js -d src/test/fixture/design.json -o testoutput'
         ).toString();
         output = output.replace(/info: Loaded generator .*github-app.min.js.*/, '');
+        output = output.replace(
+            /warn: Please cherrypick changes from master-sgen-generated from .*/,
+            ''
+        ).replace(
+            /info: git cherry-pick .*/,
+            ''
+        );
         expect(output).toMatchSnapshot();
         execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
         execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
         execSync('npm run build', { cwd: 'testoutput', stdio: 'inherit' });
-        execSync('npm test -- -u', { cwd: 'testoutput', stdio: 'inherit' });
     });
 });
